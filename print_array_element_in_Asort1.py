3fd3c0da45b06eed9388a2b857dc9147d3a486d7
RaJBabU <30348193+rabu67@users.noreply.github.com>
2018-08-12 20:18:35 +0530

    Update print_array_element_in_Asort1.py

--- print_array_element_in_Asort1.py
+++ print_array_element_in_Asort1.py
@@ -1,8 +1,8 @@
-a=[]
+b=[]
 n=int(input("enter a range"))
 for i in range(1,n+1):
   x=int(input("enter element"))
-  a.append(x)
-a.sort()
+  b.append(x)
+b.sort()
 for i in range(0,n):
-  print(a[i],end=' ')
+  print(b[i],end=' ')
